fe7babf8f346cf3d88f813de370abae892519ab3
valerisuleo <valerio.risuleo@gmail.com>
2024-02-29 10:08:48 +0100

    add grid converter

---
+++ app/library/common/grid.tsx
@@ -0,0 +1,89 @@
+export const grid = {
+    container: 'max-w-full mx-auto p-8 sm:px-6 lg:px-8',
+    'container-fluid': 'w-full',
+    // Rows
+    row: 'p-2 w-full flex flex-wrap',
+
+    // Columns
+    col: 'p-2 w-full flex-grow',
+    'col-1': 'p-2 w-full w-1/12',
+    'col-2': 'p-2 w-full w-2/12',
+    'col-3': 'p-2 w-full w-3/12',
+    'col-4': 'p-2 w-full w-4/12',
+    'col-5': 'p-2 w-full w-5/12',
+    'col-6': 'p-2 w-full w-6/12',
+    'col-7': 'p-2 w-full w-7/12',
+    'col-8': 'p-2 w-full w-8/12',
+    'col-9': 'p-2 w-full w-9/12',
+    'col-10': 'p-2 w-full w-10/12',
+    'col-11': 'p-2 w-full w-11/12',
+    'col-12': 'p-2 w-full w-full',
+
+    // Medium (sm) breakpoints
+    'col-sm': 'p-2 w-full sm:flex-grow',
+    'col-sm-1': 'p-2 w-full sm:w-1/12',
+    'col-sm-2': 'p-2 w-full sm:w-2/12',
+    'col-sm-3': 'p-2 w-full sm:w-3/12',
+    'col-sm-4': 'p-2 w-full sm:w-4/12',
+    'col-sm-5': 'p-2 w-full sm:w-5/12',
+    'col-sm-6': 'p-2 w-full sm:w-6/12',
+    'col-sm-7': 'p-2 w-full sm:w-7/12',
+    'col-sm-8': 'p-2 w-full sm:w-8/12',
+    'col-sm-9': 'p-2 w-full sm:w-9/12',
+    'col-sm-10': 'p-2 w-full sm:w-10/12',
+    'col-sm-11': 'p-2 w-full sm:w-11/12',
+    'col-sm-12': 'p-2 w-full sm:w-full',
+
+    // Medium (md) breakpoints
+    'col-md': 'p-2 w-full md:flex-grow',
+    'col-md-1': 'p-2 w-full md:w-1/12',
+    'col-md-2': 'p-2 w-full md:w-2/12',
+    'col-md-3': 'p-2 w-full md:w-3/12',
+    'col-md-4': 'p-2 w-full md:w-4/12',
+    'col-md-5': 'p-2 w-full md:w-5/12',
+    'col-md-6': 'p-2 w-full md:w-1/2',
+    'col-md-7': 'p-2 w-full md:w-7/12',
+    'col-md-8': 'p-2 w-full md:w-8/12',
+    'col-md-9': 'p-2 w-full md:w-9/12',
+    'col-md-10': 'p-2 w-full md:w-10/12',
+    'col-md-11': 'p-2 w-full md:w-11/12',
+    'col-md-12': 'p-2 w-full md:w-full',
+
+    // Large (lg) breakpoints
+    'col-lg': 'p-2 w-full lg:flex-grow',
+    'col-lg-1': 'p-2 w-full lg:w-1/12',
+    'col-lg-2': 'p-2 w-full lg:w-2/12',
+    'col-lg-3': 'p-2 w-full lg:w-3/12',
+    'col-lg-4': 'p-2 w-full lg:w-4/12',
+    'col-lg-5': 'p-2 w-full lg:w-5/12',
+    'col-lg-6': 'p-2 w-full lg:w-1/2',
+    'col-lg-7': 'p-2 w-full lg:w-7/12',
+    'col-lg-8': 'p-2 w-full lg:w-8/12',
+    'col-lg-9': 'p-2 w-full lg:w-9/12',
+    'col-lg-10': 'p-2 w-full lg:w-10/12',
+    'col-lg-11': 'p-2 w-full lg:w-11/12',
+    'col-lg-12': 'p-2 w-full lg:w-full',
+
+    // Extra Large (xl) breakpoints
+    'col-xl': 'p-2 w-full xl:flex-grow',
+    'col-xl-1': 'p-2 w-full xl:w-1/12',
+    'col-xl-2': 'p-2 w-full xl:w-2/12',
+    'col-xl-3': 'p-2 w-full xl:w-3/12',
+    'col-xl-4': 'p-2 w-full xl:w-4/12',
+    'col-xl-5': 'p-2 w-full xl:w-5/12',
+    'col-xl-6': 'p-2 w-full xl:w-1/2',
+    'col-xl-7': 'p-2 w-full xl:w-7/12',
+    'col-xl-8': 'p-2 w-full xl:w-8/12',
+    'col-xl-9': 'p-2 w-full xl:w-9/12',
+    'col-xl-10': 'p-2 w-full xl:w-10/12',
+    'col-xl-11': 'p-2 w-full xl:w-11/12',
+    'col-xl-12': 'p-2 w-full xl:w-full',
+};
+
+// Define a type that includes all possible keys from the grid object
+type GridClassKey = keyof typeof grid;
+
+// Adjust the getCol function to use this type
+export function getCol(...classNames: GridClassKey[]): string {
+    return classNames.map((className) => grid[className]).join(' ');
+}
